f6c94745acf294eba8d42a20f7b2de6f8a9279ca
HashubYC <1055757507@qq.com>
2023-07-11 00:29:47 +0800

    return json

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+type IndexData struct {
+	Title string
+	Desc  string
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Context-Type", "application/json")
+	var indexData IndexData
+	indexData.Title = "This Title"
+	indexData.Desc = "This Desc"
+	jsonStr, _ := json.Marshal(indexData)
+	w.Write(jsonStr)
+	// w.Write([]byte("hello world"))
+}
 
 func main() {
-	http.Server{}
+	server := http.Server{
+		Addr: "127.0.0.1:8080",
+	}
+	http.HandleFunc("/", index)
+	if err := server.ListenAndServe(); err != nil {
+		log.Println(err)
+	}
 }
